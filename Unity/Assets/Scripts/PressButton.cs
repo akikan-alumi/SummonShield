94269667b6a64ddb68ed50d6f9c493f40ecc9af9
mikoshishinta <mikoshishinta@gmail.com>
2014-12-20 20:17:47 +0900

    タイトル画面のプロトタイプボタン作成終了 背景、タイトル表示など、タイトル画面素材をそれっぽく配置しました。 webプレイヤー用のサイズなので、後後変更する必要があるよ

---
+++ Unity/Assets/Scripts/PressButton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// [Easy]を押す
+	/// </summary>
+	public void PressButtonEasy(){
+		Debug.Log ("Press Easy");
+	}
+	
+	/// <summary>
+	/// [Normal]を押す
+	/// </summary>
+	public void PressButtonNormal(){
+		Debug.Log ("Press Normal");
+	}
+	
+	/// <summary>
+	/// [Hard]を押す
+	/// </summary>
+	public void PressButtonHard(){
+		Debug.Log ("Press Hard");
+	}
+	
+	/// <summary>
+	/// [Credit]を押す
+	/// </summary>
+	public void PressButtonCredit(){
+		Debug.Log ("Press Credit");
+	}
+	
+	/// <summary>
+	/// [Ranking]を押す
+	/// </summary>
+	public void PressButtonRanking(){
+		Debug.Log ("Press Ranking");
+	}
+	
+	/// <summary>
+	/// [Option]を押す
+	/// </summary>
+	public void PressButtonOption(){
+		Debug.Log ("Press Option");
+	}
+}
