12fb2e173c8a0d18fc08dffdb902ec3bb44018a1
maskedzx <deziyuu@live.jp>
2014-12-20 22:23:44 +0900

    playerスクリプト追加

---
+++ Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private bool gameOver; //ゲームオーバーフラグ
+
+	// 初期化
+	void Start () {
+		gameOver = false; //ゲームオーバーフラグを初期化
+	}
+	
+	// 毎フレームごとに更新
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collision2D col){ // 衝突判定
+		if (col.gameObject.tag == "Enemy") { // Enemyに当たったらゲームオーバーフラグをTrueに
+			gameOver = true;
+		}
+	}
+}
