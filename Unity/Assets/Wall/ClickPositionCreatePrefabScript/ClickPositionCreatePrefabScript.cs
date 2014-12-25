7914ee06013b3fa0c04ad09c631e926e55dc0cb5
houchan <phaseshift09@yahoo.co.jp>
2014-12-25 23:24:20 +0900

    修正

---
+++ Unity/Assets/Wall/ClickPositionCreatePrefabScript/ClickPositionCreatePrefabScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickPositionCreatePrefabScript : MonoBehaviour {
+	// 生成したいPrefab
+	public GameObject Prefab;
+	// クリックした位置座標
+	private Vector3 clickPosition;
+	// Use this for initialization
+	int buttunCount = 0;
+	float time = 0.0f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( buttunCount< 5){
+			// マウス入力で左クリックをした瞬間
+			if (Input.GetMouseButtonDown(0)) {
+				// ここでの注意点は座標の引数にVector2を渡すのではなく、Vector3を渡すことである。
+				// Vector3でマウスがクリックした位置座標を取得する
+				clickPosition = Input.mousePosition;
+				// Z軸修正
+				clickPosition.z = 10f;
+				// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
+				// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
+				Instantiate(Prefab, Camera.main.ScreenToWorldPoint(clickPosition), Prefab.transform.rotation);
+				//ButtunCountAdd
+				buttunCount++;
+				//time count
+
+			}
+
+		}
+
+			Destroy(Prefab,5.0f);
+		
+		//シーンから削除する場合
+		//GameObject.Destroy(Prefab);
+	}
+	//void OnTriggerEnter3D (Collider3D c){
+	//}
+}
