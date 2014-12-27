ce22692d2cd4f742c7309c3c3af4e54fb0720ea4
akikan-alumi <y_r9fj22sdf@live.jp>
2014-12-27 20:54:00 +0900

    プレイヤーをめがけて向きを変えるコードを作成

---
+++ Unity/Assets/EnemySpawn/enemysAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemysAI : MonoBehaviour {
+
+    public Transform target;
+    private Vector2 vec;
+    private float speed = 0.07f;
+
+    private void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update() {
+        straight();
+    }
+    public void straight() {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position),0.01f);
+        
+        transform.position += transform.up * -speed;
+    }
+    public void setSpeed() {
+        speed = 0.14f;
+    }
+}
