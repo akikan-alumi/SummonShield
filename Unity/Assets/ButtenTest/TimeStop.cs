6616073febc15a66370358e98c9cdbd16407b06e
mikoshishinta <mikoshishinta@gmail.com>
2014-12-29 14:12:06 +0900

    ゲーム画面での一時停止、再開するスクリプトを作成しました。 リスタートはこのゲームには不要と判断し、作っていません。 あとは、normalSceneで試しに一時停止、再開するスクリプト、ボタンをアタッチしておきました。

---
+++ Unity/Assets/ButtenTest/TimeStop.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeStop : MonoBehaviour {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[SerializeField]
+	private GameObject stopMenu;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// ボタンが押されたら一時停止するメソッド
+	/// </summary>
+	public void Stop(){
+		if (Time.timeScale != 0) {
+						
+			Time.timeScale = 0;
+			stopMenu.SetActive(true);
+			Debug.Log("Stop!");
+		} else {
+
+			Time.timeScale = 1;
+			stopMenu.SetActive(false);
+			Debug.Log("Start!");
+		}
+	}
+
+	public void GoTitle(){
+		Debug.Log ("Titleへシーン遷移します");
+		//Application.loadedLevel ("タイトルシーンへ");
+	}
+}
