8880ec6476b48d5fce996c95c3bc3bd4c19829e1
笹谷　和正 <y_r9fj22sdf@live.jp>
2015-01-01 01:31:00 +0900

    エネミーに関するスクリプトを追加（未完成）

--- Unity/Assets/EnemySpawn/script/enemy2.cs
+++ Unity/Assets/EnemySpawn/script/enemy2.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class enemy : MonoBehaviour {
+public class enemy2 : MonoBehaviour {
 	public GameObject player;
-	private Vector2 speed =new Vector2( 0f,-10f);
+	private Vector2 speed =new Vector2( 0f,-1f);
 	// Use this for initialization
 	void Start () {
+	
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		// 
-		rigidbody2D.AddForce (speed);
+	void Update () {
+		rigidbody2D.velocity = speed*5f;
 	}
 }
