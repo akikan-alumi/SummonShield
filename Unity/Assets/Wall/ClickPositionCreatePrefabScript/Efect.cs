0b169c35a8399eed6b705d53d55d7ad00219c068
maskedzx <deziyuu@live.jp>
2015-02-04 23:38:45 +0900

    エフェクト本格的に追加

---
+++ Unity/Assets/Wall/ClickPositionCreatePrefabScript/Efect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Efect : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject unrock;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+				if (collision.gameObject.CompareTag ("Enemy")) {
+						Instantiate (unrock, transform.position, transform.rotation);
+			}
+	}
+	
+	void OnAnimationFinish(){
+		Destroy (this.gameObject);
+	}
+}
