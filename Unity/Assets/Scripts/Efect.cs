418c70fdbaecb4550650174ca8c2f0a0dc3a6b59
maskedzx <deziyuu@live.jp>
2015-02-04 22:34:09 +0900

    Efectスクリプト追加

---
+++ Unity/Assets/Scripts/Efect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unrock : MonoBehaviour {
+
+	//private GameObject unrock;
+
+	void OnAnimationFinish(){
+		Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
