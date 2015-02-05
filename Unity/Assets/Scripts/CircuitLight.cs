ab96927bf6045fdaed4edf0c9fdd22e234cba310
mikoshishinta <mikoshishinta@gmail.com>
2015-02-05 17:23:15 +0900

    回路９割作成

---
+++ Unity/Assets/Scripts/CircuitLight.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircuitLight : MonoBehaviour {
+	
+	private SpriteRenderer spRenderer;
+
+	//親を取得
+	//private GameObject parent;
+	private SpriteRenderer parentSpRenderer;
+
+	void Awake()
+	{
+		//このスクリプトがアタッチされているオブジェクトのSpriteRendererコンポーネントを取得
+		spRenderer = GetComponent<SpriteRenderer> ();
+
+		//parent = transform.parent.gameObject;
+		parentSpRenderer = transform.parent.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Draw ();
+	}
+
+	private void Draw()
+	{
+		if (parentSpRenderer.enabled != true) 
+		{
+			this.spRenderer.enabled = false;
+		}else {
+			this.spRenderer.enabled = true;
+		}
+	}
+}
