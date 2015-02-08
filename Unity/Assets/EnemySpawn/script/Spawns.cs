f203b758e9b43b6475244abd0fa3453bd37341e7
YuuyaObayashi <deziyuu@live.jp>
2015-02-08 12:28:52 +0900

    スポナーをランダムな動きに変更＆スポナー起動時間を変更可能に変更

--- Unity/Assets/EnemySpawn/script/Spawns.cs
+++ Unity/Assets/EnemySpawn/script/Spawns.cs
@@ -6,16 +6,20 @@ public class Spawns : MonoBehaviour {
 
 	private int count = 0;
 	[SerializeField]
-	private int spawnSpace = 60;
+	private int spawnSpace;
 	[SerializeField]
 	private GameObject prefab;
+	[SerializeField]
+	private int startTime = 0;
+	private float nowTime = 0f;
 
 	// Update is called once per frame
 	void Update () {
 		count++;
+		nowTime += Time.deltaTime;
 
 		//スポーン間隔を満たしたら && 一時停止状態じゃないときスポーンする
-		if (count >= spawnSpace && Time.timeScale != 0) {
+		if (count >= spawnSpace && Time.timeScale != 0 && nowTime >= (float)startTime) {
 						
 			count = 0;
 						
--- Unity/Assets/Wall/prottype/Revolution.cs
+++ Unity/Assets/Wall/prottype/Revolution.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Revolution : MonoBehaviour {
 	[SerializeField]
 	private int speed = 10;
+	private float changeTime = 0f;
+	private float changeNum = 0f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,24 @@ public class Revolution : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		changeTime += Time.deltaTime;
+		changeNum = Random.Range (5f, 11f);
+		
+		//一定時間で回転方向変化
+		if(changeTime >= changeNum){
+			speed *= -1;
+			changeTime = 0f;
+		}
+
 		rigidbody2D.velocity = transform.right.normalized * speed;
 	}
+
+	void OnTriggerEnter2D(Collider2D col){ // 衝突判定
+		if (col.gameObject.CompareTag("riverce")) { // 画面下で反転
+			speed *= -1;
+			Debug.Log("SpawnerHit");
+		}
+	}
+
+
 }
