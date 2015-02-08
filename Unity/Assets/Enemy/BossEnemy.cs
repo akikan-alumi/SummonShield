22dbe1a7d5713168f5b5c143cf771ec76b439178
akikan-alumi <y_r9fj22sdf@live.jp>
2015-02-08 17:15:35 +0900

    スクリプト追加

---
+++ Unity/Assets/Enemy/BossEnemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossEnemy : MonoBehaviour {
+    [SerializeField]
+    private int Hp = 10;
+
+    public GameObject enemySield;
+    //[SerializeField]
+    //public GameObject player;
+    /*[SerializeField]
+    private float speed = 1;
+    public GameObject obj;//playerを指定*/
+    // Use this for initialization
+    void Start() {
+        //LookAt2D(obj);
+        //Move(obj);
+
+    }
+
+    public void setHP() {
+        Hp--;
+        if (Hp < 1) {
+            Destroy(this.gameObject);
+            Debug.Log("PlayerHit");
+
+        }
+        else if (Hp == 2) {
+            foreach (Transform t in this.transform) {
+                Destroy(t.gameObject);
+            }
+        }
+    }
+}
