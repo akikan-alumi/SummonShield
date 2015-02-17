f1a48d09683507af294ec089e9224fc499bb2771
maskedzx <deziyuu@live.jp>
2015-02-18 01:23:08 +0900

    タイトルにハイスコア実装

---
+++ Unity/Assets/Title/TitleHiScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleHiScore : MonoBehaviour {
+	private static float highScore;
+	private Text scoreText;
+	[SerializeField]
+	private GameObject scoreTextObj;
+	// Use this for initialization
+	void Start () {
+		highScore = PlayerPrefs.GetFloat ("highScore");
+		scoreText = scoreTextObj.GetComponent<Text> ();
+		if ((highScore == 0) || (highScore == null)){
+			scoreText.text = "-";
+		}else{		
+			scoreText.text = "" + highScore.ToString("F0");
+		}
+	}
+
+}
