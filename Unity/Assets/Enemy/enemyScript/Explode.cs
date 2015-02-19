b7a773dcee114592c11311e9fc2f993e2fd51e7c
akikan-alumi <y_r9fj22sdf@live.jp>
2015-02-19 23:54:39 +0900

    削除方法を変更

--- Unity/Assets/Enemy/enemyScript/Explode.cs
+++ Unity/Assets/Enemy/enemyScript/Explode.cs
@@ -6,10 +6,11 @@ public class Explode : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         Destroy(this.gameObject,0.5f);
-	}
-    void Update() {
         if (Time.timeScale == 0) {
             Destroy(this.gameObject);
         }
+	}
+    void Update() {
+        
     }
 }
