cbdf89fabce957c64d106a6caa161e1029931559
akikan-alumi <y_r9fj22sdf@live.jp>
2015-02-27 00:27:53 +0900

    エフェクトの生成条件変更

--- Unity/Assets/Wall/ClickPositionCreatePrefabScript/ClickPositionCreatePrefabScript.cs
+++ Unity/Assets/Wall/ClickPositionCreatePrefabScript/ClickPositionCreatePrefabScript.cs
@@ -32,24 +32,23 @@ public class ClickPositionCreatePrefabScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		targets = GameObject.FindGameObjectsWithTag("Wall");
-	
-		if( (targets.Length < spawnMax) && (stopFlg == 0)){
-			// マウス入力で左クリックをした瞬間
-			if (Input.GetMouseButtonDown(0)) {
-				// ここでの注意点は座標の引数にVector2を渡すのではなく、Vector3を渡すことである。
-				// Vector3でマウスがクリックした位置座標を取得する
-				clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				// Z軸修正
-				clickPosition.z = 0f; 
-				// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
-				// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
-				Instantiate(Prefab, clickPosition, Prefab.transform.rotation);
-                Instantiate(PrefabEf,clickPosition,PrefabEf.transform.rotation);
 
-			}
+        if (Input.GetMouseButtonDown(0)) {
+            // マウス入力で左クリックをした瞬間
+            //if (Input.GetMouseButtonDown(0)) {
+            // ここでの注意点は座標の引数にVector2を渡すのではなく、Vector3を渡すことである。
+            // Vector3でマウスがクリックした位置座標を取得する
+            clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Z軸修正
+            clickPosition.z = 0f;
+            // オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
+            // ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
+            Instantiate(PrefabEf, clickPosition, PrefabEf.transform.rotation);//エフェクト
+            if ((targets.Length < spawnMax) && (stopFlg == 0)) {
+                Instantiate(Prefab, clickPosition, Prefab.transform.rotation);//オブジェクト
+            }
 
-		}
-
+        }
 	}
 	public void getFlg (int stopFrg){
 		this.stopFlg = stopFlg;
